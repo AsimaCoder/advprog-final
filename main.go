394b89a881b80da534694d913c3e751069c42fd2
Asima <129846829+AsimaCoder@users.noreply.github.com>
2024-03-03 17:33:51 +0600

    Update main.go

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 
 // 
 const (
-	mongoURI        = "mongodb://localhost:27017"
+	mongoURI        = "mongodb+srv://assima_myrz:Ny3JCiBmeVO7TkRv@cluster.eccvy8x.mongodb.net/"
 	databaseName    = "furnitureShopDB"
 	collectionName  = "users"
 	collectionName2 = "furniture"
